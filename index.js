ea91d5c9cf012c5a3381b4da61af74d969d2ba68
Samuel Jimoh <jimohsamolumuyiwa@gmail.com>
2022-05-14 22:58:33 +0100

    Finished headerparser project

--- index.js
+++ index.js
@@ -5,10 +5,12 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
+var requestIp = require('request-ip');
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { request } = require('express');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -21,13 +23,18 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  let clientIp = requestIp.getClientIp(req);
+  let clientSoftware = req.headers["user-agent"];
+  let clientLanguage = req.headers["accept-language"]
+  res.json({ ipaddress: clientIp, software: clientSoftware, language: clientLanguage});
 });
 
 
 
+
+
 // listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
